25d1fd63ec7bea4fbf70d3b924b940d7b0724fe8
yukai-w <yukai.w@outlook.com>
2019-09-27 14:40:07 +0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
 ];
 
+// 因部署在netlify站点，有刷新问题。只能先使用hash路由
+const ROUTER_CONFIG: ExtraOptions = {
+  useHash: true
+};
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, ROUTER_CONFIG)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
